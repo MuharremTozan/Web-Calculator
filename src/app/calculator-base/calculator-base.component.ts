21583b94bc25ddda6247098646afc06d45e59e84
Muharrem Tozan <muharremtozan@hotmail.com>
2024-08-04 17:28:17 +0300

    header and header list added

---
+++ src/app/calculator-base/calculator-base.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-base',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator-base.component.html',
+  styleUrl: './calculator-base.component.css'
+})
+export class CalculatorBaseComponent {
+
+}
